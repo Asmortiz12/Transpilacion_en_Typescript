16cac4a5c424c097000a3eb54b7dd1ef90d39efe
Asmortiz12 <asm.ortiz@yavirac.edu.ec>
2023-05-04 21:29:21 -0500

    Transpilacion en Typescript

---
+++ main.ts
@@ -0,0 +1,47 @@
+
+let persona= prompt("ingrese su nombre");
+let edad= prompt("ingrese su edad");
+let nCarrera= prompt("ingrese el nombre de la carrera que esta siguiendo");
+let nivel= prompt("ingrese su nivel");
+
+function saludar(persona: string, edad: string, nCarrera: string, nivel: string): void {
+    document.write(`<br>Hola ${persona}<br> Con edad: ${edad}<br> Que estudia ${nCarrera}<br> Que esta en: ${nivel}<br>`);
+}
+if (persona !== null && edad !== null && nCarrera !== null && nivel !== null) {
+    saludar(persona, edad, nCarrera, nivel);
+  } else {
+    console.log("Error: se ingresó un valor inválido.");
+  }
+console.log("hola clase de de programacion visual");
+document.write("hola clase de programacion desde la ventana<br>");
+console.log("Alexander Ortiz");
+document.write("Alexander Ortiz<br>"); 
+class Cliente {
+    public id:number;
+    public nombre:string;
+    public telefono:string;
+    
+    constructor(nombre:string, telefono:string, id:number){
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    };
+};
+
+var Cliente1:Cliente = new Cliente("Juan","0925456564556", 56);
+function saludarAjuan(Cliente1 : Cliente){
+    return ("Hola Cliente: ")+Cliente1.nombre;
+}
+let saludo = saludarAjuan (Cliente1)
+document.write("id Cliente: "+Cliente1.id+"<br>", "Nombre del cliente: "+Cliente1.nombre+"<br>", Cliente1.telefono+"<br>");
+document.write(saludo)
+console.log(Cliente1)
+console.log(persona)
+console.log(edad)
+console.log(nCarrera)
+console.log(nivel)
+
+
+
+
+
